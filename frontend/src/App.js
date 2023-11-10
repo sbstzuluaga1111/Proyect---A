d5497691d99d98658bd7273628b431ea4d4745c6
Sebastián Andrés Zuluaga Salgado <133683120+sbstzuluaga1111@users.noreply.github.com>
2023-11-10 14:01:27 +0000

    ✨ New Feature: V2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Nav from './components/Nav';
+
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Hola</h1>
+       <img className='imagenLog' src='https://avatars.githubusercontent.com/u/133683120?s=400&u=18a8b9f43e41b9ea6741aacc954b45f36bcf396d&v=4' alt=""></img>
       </header>
     </div>
   );

2998ecd6c08cdd2f5e5fc1a9464de6fd47810396
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-12-20 21:15:32 -0500

    Reset

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Nav from './components/Nav';
-
-import Home from './components/Home';
-import Contact from './components/Contact';
-import Product from './components/Productos';
 
 function App() {
   return (
-    <Router>
-      <Nav />
-      <div className='separate'>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/products" component={Product} />
-      </Switch>
-      </div>
-      <div className='App-headerr'>
-        <h1>Footer</h1>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
